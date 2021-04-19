6a8770e1c8e137f2a1f8e9ab0a031d51c5424b3f
NightWolf808 <65489492+NightWolf808@users.noreply.github.com>
2021-04-20 00:02:01 +0430

    Update and rename shisrc000.php to shisrcshi1.php

--- src/shi/shisrc000.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Shisrc\Shi;
-class Shisrc000
-{
-  public function __construct()
-  {
-    echo 'class : shi shi shi Shisrc000';
-  }
-}
---
+++ src/shi/shisrcshi1.php
@@ -0,0 +1,9 @@
+<?php
+namespace Source\Shi;
+class shisrcshi1
+{
+  public function __construct()
+  {
+    echo 'class : Source shisrcshi1';
+  }
+}
